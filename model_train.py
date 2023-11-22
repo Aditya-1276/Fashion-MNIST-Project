9ee0841c93d1975946720908061ce46f4f32af2c
ARCHIT GUPTA <archit.gupta@tamu.edu>
2023-11-22 09:11:57 -0600

    python changes

--- model_train.py
+++ model_train.py
@@ -10,14 +10,17 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.datasets import fashion_mnist
 
-fashion_train_df = pd.read_csv('data/raw/fashion-mnist_train.csv',sep=',')
-fashion_test_df = pd.read_csv('data/raw/fashion-mnist_test.csv', sep = ',')
+# fashion_train_df = pd.read_csv('data/raw/fashion-mnist_train.csv',sep=',')
+# fashion_test_df = pd.read_csv('data/raw/fashion-mnist_test.csv', sep = ',')
 
-train_labels = fashion_train_df.iloc[:, 0].to_numpy()
-train_features = fashion_train_df.iloc[:, 1:].to_numpy()
-test_labels = fashion_test_df.iloc[:, 0].to_numpy()
-test_features = fashion_test_df.iloc[:, 1:].to_numpy()
+(train_features, train_labels), (test_features, test_labels) = fashion_mnist.load_data()
+
+# train_labels = fashion_train_df.iloc[:, 0].to_numpy()
+# train_features = fashion_train_df.iloc[:, 1:].to_numpy()
+# test_labels = fashion_test_df.iloc[:, 0].to_numpy()
+# test_features = fashion_test_df.iloc[:, 1:].to_numpy()
 
 train_features, val_features, train_labels, val_labels = train_test_split(train_features, train_labels, test_size=10000, random_state=42)
 
@@ -25,9 +28,13 @@ train_features = train_features/255
 test_features = test_features/255
 val_features = val_features/255
 
+
+
 train_features = train_features.reshape(train_features.shape[0], * (28, 28, 1))
 test_features = test_features.reshape(test_features.shape[0], * (28, 28, 1))
 val_features = val_features.reshape(val_features.shape[0], * (28, 28, 1))
+
+# print(train_features.shape)
 
 cnn3 = load_model('src/models/model_cnn3.h5')
 
